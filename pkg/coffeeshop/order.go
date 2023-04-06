8f31d44cd2b2a0547e55cb0b8ba9a54295464b88
Bill Ataras <bataras@gmail.com>
2023-04-06 00:21:35 -0700

    remove order state. overkill for now

--- pkg/coffeeshop/order.go
+++ pkg/coffeeshop/order.go
@@ -31,14 +31,6 @@ func OrderStrengths() []Strength {
 	return []Strength{NormalStrength, MediumStrength, LightStrength}
 }
 
-type OrderState int
-
-const (
-	NeedsGrinder OrderState = iota
-	ReadyToBrew
-	Completed
-)
-
 type OrderDoneCB func()
 
 type Order struct {
@@ -46,7 +38,6 @@ type Order struct {
 	BeanType             string
 	OuncesOfCoffeeWanted int
 	StrengthWanted       Strength
-	State                OrderState // todo remove order state or use it with a single order queue
 	done                 chan<- *model.Receipt
 	grinder              *Grinder
 	brewer               *Brewer
@@ -58,7 +49,6 @@ var orderCount atomic.Int32
 func NewOrder(receipts chan<- *model.Receipt, notifyComplete OrderDoneCB) *Order {
 	return &Order{
 		OrderNumber:    int(orderCount.Add(1)),
-		State:          NeedsGrinder,
 		done:           receipts,
 		notifyComplete: notifyComplete,
 	}
